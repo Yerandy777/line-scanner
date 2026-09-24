eb9ecce9f8d17a1f6c41f6a9a169b835d0dd0293
Yerandy777 <soleryerandy7@gmail.com>
2026-09-24 14:49:59 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,61 +1,24 @@
-const CACHE='line-scanner-pro-v40-20260924-1520';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='line-scanner-pro-v41-20260924-1830';
+const SHELL=['./','./index.html'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then(c => c.addAll(SHELL))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener('install',event=>{
+ event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()));
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(
-        keys
-          .filter(k => k.startsWith('line-scanner-pro-') && k !== CACHE)
-          .map(k => caches.delete(k))
-      ))
-      .then(() => self.clients.claim())
-  );
+self.addEventListener('activate',event=>{
+ event.waitUntil(caches.keys().then(keys=>Promise.all(
+  keys.filter(k=>k.startsWith('line-scanner-pro-')&&k!==CACHE).map(k=>caches.delete(k))
+ )).then(()=>self.clients.claim()));
 });
-
-self.addEventListener('message', event => {
-  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  const req = event.request;
-  if (req.method !== 'GET') return;
-
-  const url = new URL(req.url);
-  if (url.origin !== self.location.origin) return;
-
-  if (
-    req.mode === 'navigate' ||
-    url.pathname.endsWith('/index.html') ||
-    url.pathname.endsWith('/')
-  ) {
-    event.respondWith(
-      fetch(req, {cache: 'no-store'})
-        .then(res => {
-          const copy = res.clone();
-          caches.open(CACHE).then(c => c.put('./index.html', copy)).catch(() => {});
-          return res;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(req).then(hit =>
-      hit || fetch(req).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
-        return res;
-      })
-    )
-  );
+self.addEventListener('fetch',event=>{
+ const req=event.request;
+ if(req.method!=='GET')return;
+ const url=new URL(req.url);
+ if(url.origin!==self.location.origin)return;
+ if(req.mode==='navigate'||url.pathname.endsWith('/index.html')||url.pathname.endsWith('/')){
+  event.respondWith(fetch(req,{cache:'no-store'}).then(res=>{
+   const copy=res.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)).catch(()=>{}); return res;
+  }).catch(()=>caches.match('./index.html')));
+  return;
+ }
+ event.respondWith(caches.match(req).then(hit=>hit||fetch(req)));
 });
